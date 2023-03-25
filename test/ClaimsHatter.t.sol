d805c4d055ab462d7cac73895013c58444c39c25
spengrah <code@spengrah.xyz>
2023-03-25 14:51:30 -0500

    remove counter.sol refs

---
+++ test/ClaimsHatter.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/ClaimsHatter.sol";
+
+contract ClaimsHatterTest is Test {
+
+  function setUp() public { }
+}
