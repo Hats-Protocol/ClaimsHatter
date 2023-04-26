f9129d28806c815361bfd4196cc50d742055b2c8
spengrah <code@spengrah.xyz>
2023-04-25 22:30:34 -0500

    test gas report action

--- src/ClaimsHatter.sol
+++ src/ClaimsHatter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// import { console2 } from "forge-std/Test.sol"; // remove before deploy
+import { console2 } from "forge-std/Test.sol"; // remove before deploy
 import { ClaimsHatterFactory } from "src/ClaimsHatterFactory.sol";
 import { IHats } from "hats-protocol/Interfaces/IHats.sol";
 import { Clone } from "solady/utils/Clone.sol";
