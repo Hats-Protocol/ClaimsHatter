8a725c25f236250235cfee8bec9dabc6252f91ef
Ido <gershido@gmail.com>
2023-07-30 22:52:20 +0300

    update hats contract address

--- script/ClaimsHatter.s.sol
+++ script/ClaimsHatter.s.sol
@@ -9,11 +9,11 @@ import { ClaimsHatterFactory } from "src/ClaimsHatterFactory.sol";
 contract DeployFactory is Script {
   ClaimsHatterFactory factory;
   ClaimsHatter implementation;
-  IHats public constant hats = IHats(0x9D2dfd6066d5935267291718E8AA16C8Ab729E9d); // v1.hatsprotocol.eth
+  IHats public constant hats = IHats(0x3bc1A0Ad72417f2d411118085256fC53CBdDd137); // v1.hatsprotocol.eth
   bytes32 internal constant SALT = bytes32(abi.encode(0x4a75)); // ~ H(4) A(a) T(7) S(5)
 
   // variables with defaul values
-  string public version = "0.3.0"; // increment with each deploy
+  string public version = "0.4.0"; // increment with each deploy
   bool verbose = true;
 
   /// @notice Overrides default values
