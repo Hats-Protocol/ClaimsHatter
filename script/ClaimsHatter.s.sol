8a3500adeffa7f9cdb360bfd6a06d2dbefc6d70b
Ido <gershido@gmail.com>
2023-08-20 20:15:41 +0300

    use the  hats module factory

--- script/ClaimsHatter.s.sol
+++ script/ClaimsHatter.s.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.13;
 
 import { Script, console2 } from "forge-std/Script.sol";
-import { IHats } from "hats-protocol/Interfaces/IHats.sol";
 import { ClaimsHatter } from "src/ClaimsHatter.sol";
-import { ClaimsHatterFactory } from "src/ClaimsHatterFactory.sol";
+import { HatsModuleFactory, deployModuleInstance } from "lib/hats-module/src/utils/DeployFunctions.sol";
 
-contract DeployFactory is Script {
-  ClaimsHatterFactory factory;
+contract Deploy is Script {
   ClaimsHatter implementation;
-  IHats public constant hats = IHats(0x3bc1A0Ad72417f2d411118085256fC53CBdDd137); // v1.hatsprotocol.eth
   bytes32 internal constant SALT = bytes32(abi.encode(0x4a75)); // ~ H(4) A(a) T(7) S(5)
 
   // variables with defaul values
@@ -29,14 +26,11 @@ contract DeployFactory is Script {
     vm.startBroadcast(deployer);
     // deploy the implementation
     implementation = new ClaimsHatter{ salt: SALT }(version);
-    // deploy the contract
-    factory = new ClaimsHatterFactory{ salt: SALT }(implementation, hats, version);
     vm.stopBroadcast();
 
     if (verbose) {
       console2.log("implementation", address(implementation));
-      console2.log("factory", address(factory));
     }
   }
-  // forge script script/ClaimsHatter.s.sol:DeployFactory -f mainnet --broadcast --verify
+  // forge script script/ClaimsHatter.s.sol:Deploy -f mainnet --broadcast --verify
 }
